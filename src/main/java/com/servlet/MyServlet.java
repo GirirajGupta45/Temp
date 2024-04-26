d3cdad2f4655ee8f6553dd73ee61c715c64b87c9
GirirajGupta45 <146866177+GirirajGupta45@users.noreply.github.com>
2024-04-26 19:46:46 +0530

    MyServlet Message

--- src/main/java/com/servlet/MyServlet.java
+++ src/main/java/com/servlet/MyServlet.java
@@ -1,6 +1,8 @@
 package com.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,7 +21,8 @@ public class MyServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter pw=response.getWriter();
+		pw.println("Hello World");
 	}
 
 }
